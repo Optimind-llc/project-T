dbb726e2d7992574d886806a8181d638f2895c1a
shiichi <s.shiichi311041@gmail.com>
2016-09-10 01:52:07 +0900

    create migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,34 +14,10 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('email')->unique();
+            $table->string('name')->unique();
             $table->string('password', 60)->nullable();
 
-            $table->string('device_id')->nullable();
-            $table->string('device_os')->nullable();
-            $table->string('token', 60)->unique();
-
-            $table->tinyInteger('status')->default(0)->unsigned();
-            $table->string('confirmation_code');
-            $table->boolean('confirmed')->default(config('access.users.confirm_email') ? false : true);
-            $table->rememberToken();
-
-            $table->string('family_name');
-            $table->string('given_name');
-            $table->string('family_name_yomi')->nullable();
-            $table->string('given_name_yomi')->nullable();
-            $table->string('phone')->nullable();
-            $table->tinyInteger('sex')->nullable();
-            $table->date('birthday')->nullable();
-            $table->string('personal_id')->nullable();
-            $table->string('postal_code')->nullable();
-            $table->string('state')->nullable();
-            $table->string('city')->nullable();
-            $table->string('street')->nullable();
-            $table->string('building')->nullable();
-
-            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
-            $table->timestamp('updated_at');
+            $table->timestamps();
             $table->softDeletes();
         });
     }
---
+++ database/migrations/2016_09_01_000000_create_tables_connected_with_part_types.php
@@ -0,0 +1,132 @@
+<?php
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateTablesConnectedWithPartTypes extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return  void
+     */
+    public function up()
+    {
+        Schema::create('vehicles', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('number')->unique();
+            $table->string('name')->unique();
+            $table->timestamps();
+        });
+
+        Schema::create('divisions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name')->unique();
+            $table->timestamps();
+        });
+
+        Schema::create('part_types', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('pn');     //品番:67119
+            $table->string('name');    //品名
+            $table->integer('vehicle_id')->unsigned();
+            $table->integer('division_id')->unsigned();
+            $table->integer('molding_image_id')->unsigned();
+            $table->integer('holing_image_id')->unsigned();
+            $table->integer('bonding_image_id')->unsigned();
+            $table->timestamps();
+
+            /**
+             * Add Foreign
+             */
+            $table->foreign('vehicle_id')
+                ->references('id')
+                ->on('vehicles')
+                ->onUpdate('cascade')
+                ->onDelete('restrict');
+
+            $table->foreign('division_id')
+                ->references('id')
+                ->on('divisions')
+                ->onUpdate('cascade')
+                ->onDelete('restrict');
+        });
+
+        Schema::create('process_types', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name')->unique();
+            $table->timestamps();
+        });
+
+        Schema::create('figures', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('path')->unique();
+            $table->string('type');
+            $table->timestamps();
+        });
+
+        Schema::create('figure_partTypes', function (Blueprint $table) {
+            $table->integer('part_type_id')->unsigned();
+            $table->integer('process_type_id')->unsigned();
+            $table->integer('figure_id')->unsigned()->unique();
+
+            /**
+             * Add Foreign
+             */
+            $table->foreign('part_type_id')
+                ->references('id')
+                ->on('part_types')
+                ->onUpdate('cascade')
+                ->onDelete('restrict');
+
+            $table->foreign('process_type_id')
+                ->references('id')
+                ->on('process_types')
+                ->onUpdate('cascade')
+                ->onDelete('restrict');
+
+            $table->foreign('figure_id')
+                ->references('id')
+                ->on('figures')
+                ->onUpdate('cascade')
+                ->onDelete('restrict');
+
+            /**
+             * Add Primary
+             */
+            $table->primary(['part_type_id', 'process_type_id']);
+        });
+
+        Schema::create('holes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('figure_id')->unsigned();
+            $table->integer('number');      //順番
+            $table->integer('horizontal');  //横軸
+            $table->integer('vertical');    //縦軸
+            $table->timestamps();
+
+            /**
+             * Add Foreign
+             */
+            $table->foreign('figure_id')
+                ->references('id')
+                ->on('figures')
+                ->onUpdate('cascade')
+                ->onDelete('restrict');
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return  void
+     */
+    public function down()
+    {
+        Schema::drop('part_types');
+        Schema::drop('holes');
+        Schema::drop('images');
+        Schema::drop('divisions');
+        Schema::drop('vehicles');
+    }
+}
